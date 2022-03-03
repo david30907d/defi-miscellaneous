713186877b7f4a122c1b389e1cdfb823d7a5549b
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2022-03-03 21:51:19 +0800

    add restake.py to optimize my yield

---
+++ restake.py
@@ -0,0 +1,15 @@
+def restake(money, apr, hyper_param_day, cost):
+    return money + money * apr / 365 * hyper_param_day - cost
+
+
+how_many_money_your_farm = input("how_many_money_are_your_farming?")
+result = []
+for hyper_param_day in range(1, 100):
+    money = how_many_money_your_farm
+    for _ in range(int(100 / hyper_param_day)):
+        money = restake(money, 0.7, hyper_param_day, 1)
+    result.append({"hyper_param_day": hyper_param_day, "money": money})
+print(
+    "hyper_param_day is the day that you should restake to optimize your yield!",
+    sorted(result, key=lambda x: -x["money"])[:10],
+)
